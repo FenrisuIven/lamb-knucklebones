ba9f51e5f60d8238f38adb26288bf8f052176096
FenrisuIven <120093556+FenrisuIven@users.noreply.github.com>
2025-01-06 01:13:29 +0200

    fix board scoring issue

--- src/entities/turnsController/turnsController.js
+++ src/entities/turnsController/turnsController.js
@@ -2,62 +2,58 @@ import { Board } from "../board/board.js";
 import { Dice } from "../dice/dice.js";
 import { createNode } from "../../view/nodeGeneration/nodes.js";
 
-// looks as good as I can make it
-
 export class TurnsController {
     idx = 0;
-    player  = null;
-    opponent = null;
+    players = {};
     dice = null;
-    playerNode = null;
-    opponentNode = null;
     
     constructor() {
         this.initPlayers();
         this.addEventListeners();
     }
     initPlayers() {
-        const firstMove = Math.floor(Math.random() * 10)        
-        this.dice = new Dice(`dice controller-${this.idx}`)
-        this.player = new Board({
-            idx: this.idx,
-            dice: this.dice, 
-            blockInput: firstMove % 2 === 0
-        });
-        this.opponent = new Board({
-            idx: this.idx,
-            dice: this.dice,
-            blockInput: firstMove % 2 !== 0
-        });
-        this.playerNode = this.initPlayerNode();
-        this.opponentNode = this.initOpponentNode();
+        const firstMove = Math.floor(Math.random() * 10);
+        this.dice = new Dice(`dice controller-${this.idx}`);
+        
+        this.players = {
+            player: new Board({
+                idx: this.idx,
+                dice: this.dice,
+                blockInput: firstMove % 2 === 0
+            }),
+            opponent: new Board({
+                idx: this.idx,
+                dice: this.dice,
+                blockInput: firstMove % 2 !== 0
+            })
+        };
+
+        this.players.playerNode = this.initPlayerNode(true);
+        this.players.opponentNode = this.initPlayerNode(false);
+        
         console.log('first to move: ' + (firstMove % 2 === 0 ? 'opponent' : 'player'));
     }
     addEventListeners() {
-        this.opponent.node.addEventListener('click', (event) => this.opponentMove(event))
-        this.player.node.addEventListener('click', (event) => this.playerMove(event))
+        this.players.opponentNode.addEventListener('click', (event) => this.handleMoveAttempt(event, { 
+            target: this.players.opponent,
+            playerMove: false
+        }))
+        this.players.playerNode.addEventListener('click', (event) => this.handleMoveAttempt(event, {
+            target: this.players.player,
+            playerMove: true
+        }))
     }
     
-    // this can definitely be refactored, idk how
-    playerMove(event) {
-        if(!this.player.handleInputAttempt(event)) return;
-        
-        this.updateDisplayScore(this.playerNode, this.player._currentScore);
-        
-        this.opponent._blockInput = false;
-        this.player._blockInput = true;
-        if (!this.player.freeCells()) return this.endGame();
+    handleMoveAttempt(event, ctx) {
+        if(!ctx.target.handleInputAttempt(event)) return;
+        const targetNode = ctx.playerMove ? this.players.playerNode : this.players.opponentNode;
+
+        this.updateDisplayScore(targetNode, ctx.target.currentScore);
+
+        this.players.opponent.blockInput = !ctx.playerMove;
+        this.players.player.blockInput = ctx.playerMove;
+        if (!ctx.target.freeCells()) return this.endGame();
     }
-    opponentMove(event) {
-        if(!this.opponent.handleInputAttempt(event)) return;
-        
-        this.updateDisplayScore(this.opponentNode, this.opponent._currentScore);
-        
-        this.player._blockInput = false;
-        this.opponent._blockInput = true;
-        if (!this.opponent.freeCells()) return this.endGame();
-    }
-    // ------------------------------------------
     
     updateDisplayScore(target, score) {
         const targetName = target.className.split('-')[0];
@@ -68,32 +64,27 @@ export class TurnsController {
         target.replaceChild(span, target.children[Number(targetName === 'player')]);
     }
     
-    // same here, only I fell like I can probably refactor this, so it takes less space
-    // I am just lazy to do so rn )0
-    initPlayerNode() {
+    initPlayerNode(targetIsPlayer) {
+        const { player, opponent } = this.players;
+        const name = targetIsPlayer ? 'player' : 'opponent';
+        
         return createNode('div',{
-                className: 'player-container',
-                children: [ this.player.node, 
+                className: `${name}-container`,
+                children: [ targetIsPlayer ? player.node : null, 
                     createNode('span', {
-                        textContent: 'player score: '
-                    }) 
+                        textContent: `${name} score: `
+                    }) ,
+                    !targetIsPlayer ? opponent.node : null,
                 ],
             })
     }
-    initOpponentNode() {
-        return createNode('div', {
-            className: 'opponent-container',
-            children: [ createNode('span', {
-                textContent: 'opponent score: '
-            }), this.opponent.node ]
-        })
-    }
-    // -------------------------------------------------------------------------------
     
     endGame() {
-        const winnerName = this.player._currentScore > this.opponent._currentScore ? 'player' : 'opponent'
+        const { player, opponent } = this.players;
+        const winnerName = player.currentScore > opponent.currentScore ? 'player' : 'opponent'
         console.log('Game ended! Winner is: ' + winnerName);
-        this.player._blockInput = true;
-        this.player._blockInput = true;
+        
+        player.blockInput = true;
+        opponent.blockInput = true;
     }
 }
